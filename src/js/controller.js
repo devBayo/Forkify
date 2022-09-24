4f99125e27520278b648f0950be27fffed331faf
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-09-24 12:32:13 +0100

    created showRecipe && fetched recipe from forkify api

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,35 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ////////////////////////////////////
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc40'
+      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+    console.log(res);
+    console.log(data);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      cookingTime: recipe.cooking_time,
+      imageUrl: recipe.image_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceUrl: recipe.source_url,
+      title: recipe.title,
+    };
+    console.log(recipe);
+
+    if (!res.ok) {
+      throw new Error(`${data.message} (${res.status})`);
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+};
+
+showRecipe();
