f782745d3813aaaad349c36dc15ffa2efe592750
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-09-26 20:16:33 +0100

    created config -- refactored API_URL

--- src/js/model.js
+++ src/js/model.js
@@ -1,12 +1,12 @@
+import { API_URL } from './config.js';
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
+    const res = await fetch(`${API_URL}/${id}`);
     // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
     const data = await res.json();
 
