c4488c9c08f15f80c86daeeb6c946f661d0a2c9c
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-09-27 03:22:16 +0100

    refactored timeout seconds

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,3 +1,5 @@
+import { TIMEOUT_SEC } from './config.js';
+
 const timeout = function (s) {
   return new Promise(function (_, reject) {
     setTimeout(function () {
@@ -8,7 +10,7 @@ const timeout = function (s) {
 
 export const getJSON = async function (url) {
   try {
-    const res = await Promise.race([fetch(url), timeout(10)]);
+    const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
 
     const data = await res.json();
 
