2dd7136a40bd3442c1378e4d45860b05c641b136
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-09-27 04:21:49 +0100

    created loadSearchResults

--- src/js/model.js
+++ src/js/model.js
@@ -3,6 +3,10 @@ import { getJSON } from './helper.js';
 
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
 };
 
 export const loadRecipe = async function (id) {
@@ -26,3 +30,24 @@ export const loadRecipe = async function (id) {
     throw err;
   }
 };
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    state.search.results = data.data.recipes.map(recipe => {
+      return {
+        id: recipe.id,
+        title: recipe.title,
+        publisher: recipe.publisher,
+        imageUrl: recipe.image_url,
+      };
+    });
+
+    console.log(state.search.results);
+  } catch (err) {
+    throw err;
+  }
+};
+
+loadSearchResults('pizza');
