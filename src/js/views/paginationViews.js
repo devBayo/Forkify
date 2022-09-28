dd42be22e1bccaa590514c1a517fc3b8a2b64b34
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-09-28 13:14:19 +0100

    pagination --- checking for current page

--- src/js/views/paginationViews.js
+++ src/js/views/paginationViews.js
@@ -8,27 +8,32 @@ class PaginationView extends View {
     const totalPages = Math.ceil(
       this._data.results.length / this._data.resultPerPage
     );
-    console.log(totalPages);
+    const currentPage = this._data.currentPage;
 
     // Page 1 and there are other pages
-    if (this._data.currentPage === 1 && totalPages > 1) {
-      return 'Page 1 and other pages';
+    if (currentPage === 1 && totalPages > 1) {
+      return `
+        <button class="btn--inline pagination__btn--next">
+          <span>Page ${currentPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>
+      `;
     }
-    // Page 1 and no other pages
-    if (this._data.currentPage === totalPages && totalPages === 1) {
-      return 'Page 1 only';
-    }
+
     // Last page
-    if (this._data.currentPage === totalPages && totalPages > 1) {
+    if (currentPage === totalPages && totalPages > 1) {
       return 'Last Page';
     }
 
     // Other pages
-    if (this._data.currentPage < totalPages) {
+    if (currentPage < totalPages) {
       return 'Other pages';
     }
 
-    // return totalPages;
+    // Page 1 and no other pages
+    return 'Page 1 only';
   }
 }
 export default new PaginationView();
