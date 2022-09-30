bcfb832f1e57b5dfb5fc0f09214a9afc19e10c51
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-09-30 10:46:45 +0100

    refactored bookmarkview and resultsview

--- src/js/views/bookmarkViews.js
+++ src/js/views/bookmarkViews.js
@@ -1,30 +1,9 @@
 import View from './views';
+import PreviewViews from './previewViews';
 import icons from 'url:../../img/icons.svg';
 
-class BookmarkView extends View {
+class BookmarkView extends PreviewViews {
   _parentEl = document.querySelector('.bookmarks__list');
   _errorMessage = 'No bookmarks yet. Find a nice recipe and bookmark it :)';
-
-  _generateMarkup() {
-    return this._data.map(this._generateMarkupPreview).join('');
-  }
-
-  _generateMarkupPreview(bookmark) {
-    const id = window.location.hash.slice(1);
-
-    return ` <li class="preview">
-    <a class="preview__link ${
-      id === bookmark.id ? 'preview__link--active' : ''
-    }" href="#${bookmark.id}">
-      <figure class="preview__fig">
-        <img src="${bookmark.imageUrl}" alt="${bookmark.title}" />
-      </figure>
-      <div class="preview__data">
-        <h4 class="preview__title">${bookmark.title}</h4>
-        <p class="preview__publisher">${bookmark.publisher}</p>
-      </div>
-    </a>
-  </li>`;
-  }
 }
 export default new BookmarkView();
