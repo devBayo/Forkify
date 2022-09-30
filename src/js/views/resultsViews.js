7c4dc24dd2899718fdde9a73a537b573d7bb8938
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-09-30 10:46:54 +0100

    refactored bookmarkview and resultsview

--- src/js/views/resultsViews.js
+++ src/js/views/resultsViews.js
@@ -1,31 +1,10 @@
 import View from './views';
+import PreviewViews from './previewViews';
 import icons from 'url:../../img/icons.svg';
 
-class ResultsView extends View {
+class ResultsView extends PreviewViews {
   _parentEl = document.querySelector('.results');
   _errorMessage =
     "We couldn't find recipes for that query. Please try another one!";
-
-  _generateMarkup() {
-    return this._data.map(this._generateMarkupPreview).join('');
-  }
-
-  _generateMarkupPreview(result) {
-    const id = window.location.hash.slice(1);
-
-    return ` <li class="preview">
-    <a class="preview__link ${
-      id === result.id ? 'preview__link--active' : ''
-    }" href="#${result.id}">
-      <figure class="preview__fig">
-        <img src="${result.imageUrl}" alt="${result.title}" />
-      </figure>
-      <div class="preview__data">
-        <h4 class="preview__title">${result.title}</h4>
-        <p class="preview__publisher">${result.publisher}</p>
-      </div>
-    </a>
-  </li>`;
-  }
 }
 export default new ResultsView();
