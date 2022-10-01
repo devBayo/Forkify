deeef0ee8632edf93554a23c190c46bad0838362
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-01 10:32:01 +0100

    uploading a custom recipe

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RESULT_PER_PAGE = 10;
+export const API_KEY = '3eebe410-1b85-4936-b1fd-73433db12139';
+export const MODAL_CLOSE_SEC = 2.5;
