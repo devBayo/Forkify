7520c13f5221e3daeaad86108444c4b7a6c203a2
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-01 14:55:18 +0100

    uploading a custom recipe

--- src/js/views/previewViews.js
+++ src/js/views/previewViews.js
@@ -19,6 +19,11 @@ export default class PreviewView extends View {
       <div class="preview__data">
         <h4 class="preview__title">${preview.title}</h4>
         <p class="preview__publisher">${preview.publisher}</p>
+       <div class="preview__user-generated ${preview.key ? '' : 'hidden'}">
+        <svg>
+          <use href="${icons}#icon-user"></use>
+        </svg>
+      </div>
       </div>
     </a>
   </li>`;
