a66a3fdc65ba8f0b4e055661b0b8c19d5622d4d1
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-03 12:59:15 +0100

    changed fraction.js to fractional

--- src/js/views/recipeViews.js
+++ src/js/views/recipeViews.js
@@ -1,6 +1,6 @@
 import View from './views.js';
 import icons from 'url:../../img/icons.svg';
-import Fraction from 'fraction.js';
+import { Fraction } from 'fractional';
 
 class RecipeView extends View {
   _parentEl = document.querySelector('.recipe');
@@ -128,9 +128,7 @@ class RecipeView extends View {
       <use href="${icons}#icon-check"></use>
     </svg>
     <div class="recipe__quantity">${
-      ingredient.quantity
-        ? new Fraction(ingredient.quantity).toFraction(true)
-        : ''
+      ingredient.quantity ? new Fraction(ingredient.quantity) : ''
     }</div>
     <div class="recipe__description">
       <span class="recipe__unit">${ingredient.unit}</span>
